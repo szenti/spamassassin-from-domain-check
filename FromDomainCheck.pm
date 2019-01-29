ee4c2f2b747f0c7105bc8dac889a44e88eabdc4c
Mátyás Szentiványi <szenti@users.noreply.github.com>
2019-01-29 21:54:25 +0100

    Handle multiple IP addresses

--- FromDomainCheck.pm
+++ FromDomainCheck.pm
@@ -4,12 +4,13 @@ use warnings;
 
 package FromDomainCheck;
 use Mail::SpamAssassin::Plugin;
-use Socket qw(inet_aton inet_ntoa);
+use Net::DNS::Resolver;
 
 my $MAIL_OK = 0;
 my $MAIL_SPAM = 1;
 my $ACTION_UNRESOLVABLE_HOSTNAME = $MAIL_OK; # if you want to drop these emails, set it to $MAIL_SPAM
 my $DOMAIN_PATTERN = q"\@([\d\w\-\.]+)";
+my $RECORD_TYPE_A = "A";
 
 my %IP_BLACKLIST = (
     "185.140.110.3" => 1,
@@ -32,26 +33,34 @@ sub new {
   return $self;
 }
 
-sub resolve_domain_to_ip {
-  my ($domain) = @_;
-  return inet_ntoa(inet_aton($domain));
+sub get_from_domain {
+  my ($message) = @_;
+  my $from_address = lc($message->get('From:addr'));
+
+  $from_address =~ /$DOMAIN_PATTERN/;
+  my $from_domain = $1; 
+}
+
+sub is_ip_blacklisted {
+  my ($ip_address) = @_;
+  return IP_BLACKLIST{$ip_address} == 1;
 }
 
 sub check_from_domain_ip {
   my ($self, $message) = @_;
-  my $from_address = lc($message->get('From:addr'));
+  my $resolver = Net::DNS::Resolver->new;
 
-  $from_address =~ /$DOMAIN_PATTERN/;
-  my $from_domain = $1;
+  my $message_from_domain = get_from_domain($message);
+  my $result = $resolver->query($message_from_domain, $RECORD_TYPE_A);
 
-  if (!gethostbyname($from_domain)) {
+  if (!$result) {
     return $ACTION_UNRESOLVABLE_HOSTNAME;
   }
 
-  my $ip_address = resolve_domain_to_ip($from_domain);
-
-  if ($IP_BLACKLIST{$ip_address}) {
-    return $MAIL_SPAM;
+  foreach my $record ($result->answer) {
+    if (is_ip_blacklisted($record->address)) {
+      return $MAIL_SPAM;
+    }
   }
 
   return $MAIL_OK;
